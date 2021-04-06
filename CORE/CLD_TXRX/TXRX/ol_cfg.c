dfd08d06ad4e370cb1d04c5a1c2cd739788c3f64
QU TAO <taoq@codeaurora.org>
2021-04-06 16:53:02 +0800

    Revert "qcacld-2.0: Reduce max thruput from 800 to 400"
    This reverts commit 4c2e89f9aab4ddfdcf6a0cc781a9ebae004ee405.
    
    Change-Id: I2f48887bb6020ea578e6a9cc75d736da156f71ee

--- CORE/CLD_TXRX/TXRX/ol_cfg.c
+++ CORE/CLD_TXRX/TXRX/ol_cfg.c
@@ -118,7 +118,7 @@ ol_pdev_handle ol_pdev_cfg_attach(adf_os_device_t osdev,
 		cfg_ctx->frame_type = wlan_frm_fmt_raw;
 	else
 		cfg_ctx->frame_type = wlan_frm_fmt_802_3;
-	cfg_ctx->max_thruput_mbps = 400;
+	cfg_ctx->max_thruput_mbps = 800;
 	cfg_ctx->max_nbuf_frags = 1;
 	cfg_ctx->vow_config = vow_config;
 	cfg_ctx->target_tx_credit = CFG_TGT_NUM_MSDU_DESC;
