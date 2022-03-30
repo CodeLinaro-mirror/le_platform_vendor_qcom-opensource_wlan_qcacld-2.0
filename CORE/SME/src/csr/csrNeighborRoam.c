2199e2cef6357699456aa6abf3497a329aa70e89
Shi Hong <quic_hongsh@quicinc.com>
2022-03-30 16:44:28 +0800

    qcacld-2.0: fix detected vulnerabilities
    Fix detected vulnerabilities which could assign 32bits value to
    8bits variable which is passed by caller.
    
    Change-Id: I1d6cb960f1b4f4b85714cba18e8fc7282874cf2b
    CRs-Fixed: 3097175

--- CORE/SME/src/csr/csrNeighborRoam.c
+++ CORE/SME/src/csr/csrNeighborRoam.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -3362,6 +3363,7 @@ eHalStatus csrNeighborRoamPerformContiguousBgScan(tpAniSirGlobal pMac,
     eHalStatus      status = eHAL_STATUS_SUCCESS;
     tCsrBGScanRequest   bgScanParams;
     tANI_U8   numOfChannels = 0, i = 0;
+    tANI_U32  tmpNumOfChannels = 0;
     tANI_U8   *channelList = NULL;
     tANI_U8   *pInChannelList = NULL;
     tANI_U8   tmpChannelList[WNI_CFG_VALID_CHANNEL_LIST_LEN];
@@ -3372,14 +3374,16 @@ eHalStatus csrNeighborRoamPerformContiguousBgScan(tpAniSirGlobal pMac,
     NEIGHBOR_ROAM_DEBUG(pMac, LOG2, FL("get valid channel list"));
 
     numOfChannels = sizeof(pMac->roam.validChannelList);
+    tmpNumOfChannels = (tANI_U32)numOfChannels;
 
     if(!HAL_STATUS_SUCCESS(csrGetCfgValidChannels(pMac,
                           (tANI_U8 *)pMac->roam.validChannelList,
-                          (tANI_U32 *) &numOfChannels)))
+                          &tmpNumOfChannels)))
     {
         smsLog(pMac, LOGE, FL("Could not get valid channel list"));
         return eHAL_STATUS_FAILURE;
     }
+    numOfChannels = (tANI_U8)tmpNumOfChannels;
     pInChannelList = pMac->roam.validChannelList;
 
     if (CSR_IS_ROAM_INTRA_BAND_ENABLED(pMac))
@@ -4317,6 +4321,7 @@ VOS_STATUS csrNeighborRoamTransitToCFGChanScan(tpAniSirGlobal pMac,
     eHalStatus  status  = eHAL_STATUS_SUCCESS;
     int i = 0;
     tANI_U8 numOfChannels = 0;
+    tANI_U32  tmpNumOfChannels = 0;
     tANI_U8   channelList[WNI_CFG_VALID_CHANNEL_LIST_LEN];
     tpCsrChannelInfo    currChannelListInfo;
     tANI_U8   scanChannelList[WNI_CFG_VALID_CHANNEL_LIST_LEN];
@@ -4521,11 +4526,13 @@ VOS_STATUS csrNeighborRoamTransitToCFGChanScan(tpAniSirGlobal pMac,
                 /* Scan all channels from non-occupied list */
                 NEIGHBOR_ROAM_DEBUG(pMac, LOG2, "Get valid channel list");
                 numOfChannels = sizeof(pMac->roam.validChannelList);
+                tmpNumOfChannels = (tANI_U32)numOfChannels;
 
                 if(HAL_STATUS_SUCCESS(csrGetCfgValidChannels(pMac,
                                 (tANI_U8 *)pMac->roam.validChannelList,
-                                (tANI_U32 *) &numOfChannels)))
+                                &tmpNumOfChannels)))
             {
+                numOfChannels = (tANI_U8)tmpNumOfChannels;
                 if (numOfChannels > WNI_CFG_VALID_CHANNEL_LIST_LEN)
                 {
                     numOfChannels = WNI_CFG_VALID_CHANNEL_LIST_LEN;
