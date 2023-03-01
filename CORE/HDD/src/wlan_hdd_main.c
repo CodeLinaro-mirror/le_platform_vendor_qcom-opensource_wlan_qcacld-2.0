3f7b39300566796dcca8f7825a2aafcb179b4ba4
Wu Gao <quic_wugao@quicinc.com>
2023-03-01 22:52:34 +0800

    qcacld-2.0: Skip to check nl_init
    Change-Id: I0b1513d7f36cc2c61a7ec6fc63ebf0d857341a19

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -17281,7 +17281,6 @@ int hdd_wlan_startup(struct device *dev, v_VOID_t *hif_sc)
    /* Initialize the nlink service */
    if (wlan_hdd_nl_init(pHddCtx) != 0) {
       hddLog(LOGP, FL("nl_srv_init failed"));
-      goto err_logging_sock;
    }
    vos_set_radio_index(pHddCtx->radio_index);
 
@@ -18307,7 +18306,6 @@ err_free_ftm_open:
 err_nl_srv:
    nl_srv_exit();
 
-err_logging_sock:
    if (VOS_FTM_MODE != hdd_get_conparam())
        wlan_hdd_logging_sock_deactivate_svc(pHddCtx);
 
