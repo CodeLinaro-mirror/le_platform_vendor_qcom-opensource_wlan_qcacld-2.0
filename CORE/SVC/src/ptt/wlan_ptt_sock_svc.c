c04af3cf2660bfcae921c7c260e89c959e5b38c3
Wu Gao <quic_wugao@quicinc.com>
2023-03-21 01:41:57 -0700

    qcacld-2.0: Use wlan_cfg80211_nla_parse to replace nla_parse
    This change uses wlan_cfg80211_nla_parse to replace nla_parse and
    supports different kernel version.
    
    Change-Id: If708c317e6c9f8bc60d748e90211690c6d826e8b
    CRs-Fixed: 3440032

--- CORE/SVC/src/ptt/wlan_ptt_sock_svc.c
+++ CORE/SVC/src/ptt/wlan_ptt_sock_svc.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2018 2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -253,7 +254,7 @@ static void ptt_cmd_handler(const void *data, int data_len, void *ctx, int pid)
 	 * audit note: it is ok to pass a NULL policy here since a
 	 * length check on the data is added later already
 	 */
-	if (nla_parse(tb, CLD80211_ATTR_MAX, data, data_len, NULL)) {
+	if (wlan_cfg80211_nla_parse(tb, CLD80211_ATTR_MAX, data, data_len, NULL)) {
 		PTT_TRACE(VOS_TRACE_LEVEL_ERROR, "Invalid ATTR");
 		return;
 	}
