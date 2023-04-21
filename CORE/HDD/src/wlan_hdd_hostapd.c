2c7f5ae8bc2109b5d1259189064cd07bc7982edd
Wu Gao <quic_wugao@quicinc.com>
2023-04-20 18:56:57 -0700

    qcacld-2.0: Use cfg80211_register_netdevice() for kernel 5.12+
    For Kernel 5.12+ in order to be in line with cfg80211 changes
    in upstream code, use cfg80211_register_netdevice() and
    cfg80211_unregister_netdevice() when request to register device is
    coming from add virtual interface.
    
    Change-Id: Ibc11d405ab8b06e170bcb261970bc4ccccafe291
    CRs-Fixed: 3472047

--- CORE/HDD/src/wlan_hdd_hostapd.c
+++ CORE/HDD/src/wlan_hdd_hostapd.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -9058,7 +9058,7 @@ VOS_STATUS hdd_register_hostapd( hdd_adapter_t *pAdapter, tANI_U8 rtnl_lock_held
             return VOS_STATUS_E_FAILURE;
          }
       }
-      if (register_netdevice(dev))
+      if (wlan_cfg80211_register_netdevice(dev))
       {
          hddLog(VOS_TRACE_LEVEL_FATAL,
                 "%s:Failed:register_netdevice", __func__);
